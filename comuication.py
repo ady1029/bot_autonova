f207795d453f2ae84426c4f06885e46e6b4d8d09
ady1029 <adrianernestorodriguez2910@gmail.com>
2026-08-15 18:01:39 -0600

    auth y tareas

--- comuication.py
+++ comuication.py
@@ -5,14 +5,32 @@ import logic
 TOKEN = '8645762118:AAHyr4XajrmKzGE41bdDB-TvICuYK530CXY'
 bot = telebot.TeleBot(TOKEN)
 chatId = '5360897820'
+sesiones_activas = set()
+
 # Comando de inicio
+
+def check_auth(message):
+    if message.from_user.id not in sesiones_activas:
+        bot.reply_to(message, "⚠️ Por favor, inicia la conversación enviando /start para autenticarte.")
+        return False
+    return True
+
+
+
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, '¡Hola! Soy Miguel, tu asistente virtual. ¿En qué puedo ayudarte hoy?')
+    user_id = message.from_user.id
+    if logic.es_usuario_autorizado(user_id):
+        sesiones_activas.add(user_id)
+        bot.reply_to(message, '¡Hola! Soy Miguel, tu asistente virtual. Ya estás autenticado. ¿En qué puedo ayudarte hoy?')
+    else:
+        bot.reply_to(message, 'Lo siento, no tienes permiso para acceder a este bot. 🚫')
 
 
 @bot.message_handler(commands=['agendar_trabajo'])
 def send_agendar_trabajo(message):
+    if not check_auth(message): return
     msg = bot.reply_to(message, """OK. Manda los datos del trabajo para añadirlo. Por favor, usa este formato:
         
     1. Cliente
@@ -21,28 +39,50 @@ def send_agendar_trabajo(message):
     4. Contacto
     5. Encargado1, Encargado2, Encargado3""")
     
-    def manejar_resultado(message):
-        resultado = logic.agregar_trabajo(message)
-        if resultado is True:
-            bot.reply_to(message, "¡Listo! El trabajo se ha guardado correctamente. ✅")
-        elif resultado is False:
-            bot.reply_to(message, "Uy, hubo un problema técnico al guardar. Inténtalo de nuevo. 😅")
-        else:
-            bot.reply_to(message, "El formato no es correcto. Asegúrate de seguir los 5 puntos. 📝")
+    
     
     # Registramos esta función interna como el siguiente paso
-    bot.register_next_step_handler(msg, manejar_resultado)
+    bot.register_next_step_handler(msg, logic.agregar_trabajo)
 
 
 @bot.message_handler(commands=['agregar_tarea'])
 def send_agregar_tarea(message):
+        if not check_auth(message): return
         msg = bot.reply_to(message, """OK. Manda los datos de la tarea para añadirlo. Por favor, usa este formato:
-    
 1. Titulo
 2. Descripcion      
 3. Responsable
 4. Estado""")
+        bot.register_next_step_handler(msg, logic.agregar_tarea)
+
+
+def manejar_resultado(resultado, message):
+        if resultado is True:
+            bot.reply_to(message, "¡Listo! Se ha guardado correctamente. ✅")
+        elif resultado is False:
+            bot.reply_to(message, "Uy, hubo un problema técnico al guardar. Inténtalo de nuevo. 😅")
+        elif resultado is None:
+            bot.reply_to(message, "El formato no es correcto. Asegúrate de seguir el formato. 📝")
+
+@bot.message_handler(commands=['mis_tareas'])
+def send_mistareas(message):
+    if not check_auth(message): return
+    user_id = message.from_user.id
+    tareas = logic.obtener_tareas_por_usuario(user_id)
+    if tareas is None:
+        bot.reply_to(message, "😕 No te encuentro en mi base de datos. ¡Habla con el administrador!")
+        return
+
+    if not tareas:
+        bot.reply_to(message, "🎉 ¡Felicidades! No tienes ninguna tarea pendiente.")
+    else:
+        mensaje = "📋 **Tus tareas pendientes:**\n\n"
+        for t in tareas:
+            mensaje += f"🔹 *{t['titulo']}*\n"
+        bot.reply_to(message, mensaje, parse_mode='Markdown')
+
 
 if __name__ == '__main__':
     print("Bot funcionando")
     bot.polling()
+
--- logic.py
+++ logic.py
@@ -1,9 +1,12 @@
 import json
 import os
-
+import comuication
 def agregar_trabajo(message):
     try:
-        lineas = [l.split(". ", 1)[-1].strip() for l in message.text.strip().split('\n')]            
+        lineas = [l.split(". ", 1)[-1].strip() for l in message.text.strip().split('\n')]    
+        if len(lineas) != 5:
+               comuication.manejar_resultado(None,message)
+               return    
         trabajo = {
                     "cliente": lineas[0],
                     "direccion": lineas[1],
@@ -17,11 +20,9 @@ def agregar_trabajo(message):
             with open(archivo, 'r', encoding='utf-8') as f:
                 try:
                     contenido = json.load(f)
-                    # AQUÍ ESTÁ EL TRUCO:
                     if isinstance(contenido, list):
                         datos = contenido
                     else:
-                        # Si era un diccionario, lo metemos dentro de una lista nueva
                         datos = [contenido]
                 except json.JSONDecodeError:
                     datos = []
@@ -29,7 +30,64 @@ def agregar_trabajo(message):
         datos.append(trabajo)
         with open(archivo, 'w', encoding='utf-8') as f:
             json.dump(datos, f, indent=4, ensure_ascii=False)
-        return True    
+        comuication.manejar_resultado(True,message)  
     except Exception as e:
         print(f"Error al escribir: {e}")
+        comuication.manejar_resultado(False, message)
+
+def agregar_tarea(message):
+    try:
+        print("entre x2")
+        lineas = [l.split(". ", 1)[-1].strip() for l in message.text.strip().split('\n')]    
+        if len(lineas) != 4:
+                    comuication.manejar_resultado(None,message)
+                    return        
+        tarea = {
+                    "titulo": lineas[0],
+                    "descripcion": lineas[1],
+                    "responsable": lineas[2],
+                    "estado": lineas[3],
+                }       
+        archivo = "tareas.json"
+        datos = []
+        if os.path.exists(archivo):
+            with open(archivo, 'r', encoding='utf-8') as f:
+                try:
+                    contenido = json.load(f)
+                    if isinstance(contenido, list):
+                        datos = contenido
+                    else:
+                        datos = [contenido]
+                except json.JSONDecodeError:
+                    datos = []
+        
+        datos.append(tarea)
+        with open(archivo, 'w', encoding='utf-8') as f:
+            json.dump(datos, f, indent=4, ensure_ascii=False)
+        comuication.manejar_resultado(True,message)   
+    except Exception as e:
+        comuication.manejar_resultado(False,message)
+
+def obtener_tareas_por_usuario(user_id):
+    with open('tareas.json', 'r', encoding='utf-8') as f:
+        tareas = json.load(f)
+    with open('usuarios.json', 'r', encoding='utf-8') as f:
+        usuarios = json.load(f)
+    usuario_info = next((u for u in usuarios if u['telegram_id'] == user_id), None)
+    if not usuario_info:
+        return None
+    tareas_pendientes = [
+        t for t in tareas 
+        if t['responsable'] == usuario_info['nombre'] and t['estado'] == 'incompleto'
+    ]
+    
+    return tareas_pendientes
+
+def es_usuario_autorizado(user_id):
+    try:
+        with open('usuarios.json', 'r') as f:
+            data = json.load(f)
+            return any(u.get("telegram_id") == user_id for u in data)
+    except (FileNotFoundError, json.JSONDecodeError):
         return False
+
